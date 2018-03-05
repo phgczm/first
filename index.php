ec8338c849886654affaf5bbeb4155ff78460ce0
miao.zhang <miao.zhang@husor.com>
2018-03-05 18:03:27 +0800

    a

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ phpinfo();
 function test1() {
     $one = 'this is mys first commit3333';
     $one = 'this is mys first commit0003';
-    $one = 'this is mys first commit0003';
+    $one = 'this is mys first commit0004';
 }
 
